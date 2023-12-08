240d67999c0d80fa2b6606acc165c19df73c93b1
gorkemsahin <gorkemsahin012@gmail.com>
2023-12-08 15:41:22 +0300

    hesap makinesi projesi tamamlanmak üzere

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const tuslar = document.getElementById('butonlar')
-const calcScreen = document.getElementById('hesap')
-
-tuslar.addEventListener('click', function (event) {
-  const clickedButtonValue = event.target.innerText
-  console.log(clickedButtonValue)
+const calcScreen = document.getElementById('screen')
+const hesapla = document.getElementById('hesapla')
+const temizle = document.getElementById('temizle')
+tuslar.addEventListener('click', function (e) {
+  const clickedButtonValue = e.target.innerText
+  // console.log(clickedButtonValue)
 
   appendValue()
   function appendValue(value) {
     calcScreen.value += clickedButtonValue
-
-    if (appendValue(value) == '') {
-      calcScreen.value += clickedButtonValue
-    }
   }
 })
+
+hesapla.addEventListener('click', hesap)
+
+function hesap() {
+  calcScreen.value = eval(calcScreen.value)
+}
+
+temizle.addEventListener('click', temizleme)
+
+function temizleme() {
+  calcScreen.value = ''
+}
